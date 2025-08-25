7d6f4606e26867de9743ae3d3a4c839a18e6ba18
Zeeton <146897841+Dhanush-67@users.noreply.github.com>
2025-08-25 00:56:00 -0400

    Added basic math operation functions and set up the foundations for the calculator UI

--- script.js
+++ script.js
@@ -11,5 +11,27 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
+  if (num2 === 0) {
+    return "Math error";
+  }
   return num1 / num2;
+}
+
+let num1;
+let num2;
+let operation;
+
+function operate(num1, operation, num2) {
+  if (operation === "+") {
+    return add(num1, num2);
+  }
+  if (operation === "-") {
+    return subtract(num1, num2);
+  }
+  if (operation === "*") {
+    return multiply(num1, num2);
+  }
+  if (operation === "/") {
+    return divide(num1, num2);
+  }
 }
